1539559b53c423a2d3468b2d785a3171a817c656
Sayfullah Eid <sayfullaheid@gmail.com>
2019-01-19 19:07:32 -0500

    Removed double definitions of QueryTypes
    Signed-off-by: Sayfullah Eid <sayfullaheid@gmail.com>

--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -1,2 +1,11 @@
-class ProductType
+# frozen_string_literal: true
+
+module Types
+  class ProductType < Types::BaseObject
+    name 'Product'
+    field :id, !types.ID
+    field :title, !types.String
+    field :price, !types.Float
+    field :inventory_count, !types.Int
+  end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,14 +4,13 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-    Types::QueryType = GraphQL::ObjectType.define do
-      name 'Query'
 
-      # queries are just represented as fields
-      field :allProducts, !types[Types::ProductType] do
-        # resolve would be called in order to fetch data for that field
-        resolve ->(_obj, _args, _ctx) { Product.all }
-      end
+    name 'Query'
+
+    # queries are just represented as fields
+    field :allProducts, !types[Types::ProductType] do
+      # resolve would be called in order to fetch data for that field
+      resolve ->(_obj, _args, _ctx) { Product.all }
     end
   end
 end
